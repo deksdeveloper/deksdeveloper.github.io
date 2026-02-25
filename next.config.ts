5d156ce3ec291d47e977da89bf7ed8bb83d4aa39
Deks <efedc191@gmail.com>
2026-02-25 22:56:20 +0300

    fix: next config for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  trailingSlash: true,
 }
 
-module.exports = nextConfig
+export default nextConfig;
